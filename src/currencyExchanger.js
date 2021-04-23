45f553c7403af10b1fcf14bcd1a41acdbe7948b5
Sammai Gutierrez <sammaigutierrez@gmail.com>
2021-04-23 12:06:03 -0700

    Struggle to test first call-WIP.

--- src/currencyExchanger.js
+++ src/currencyExchanger.js
@@ -13,18 +13,18 @@ export default class ExchangeNow{
 
   }
 
-  static getRwanda(){
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-    .then(function(response){
-      if(!response.ok){
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error){
-      return error;
-    });
-  }
+  // static getRwanda(){
+  //   return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  //   .then(function(response){
+  //     if(!response.ok){
+  //       throw Error(response.statusText);
+  //     }
+  //     return response.json();
+  //   })
+  //   .catch(function(error){
+  //     return error;
+  //   });
+  // }
 
   // more countries should go below
 }
--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeNow from './currencyExchanger.js';
 
+function getNorwayElements(conversion, response){
+  if (conversion){
+    $("#showConversion").text(`${conversion}`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+function getRwandaElements(){
 
+}
 
 
 $(document).ready(function(){
   $("form.exchanger").submit(function(event){
     event.preventDefault();
+
+    let usAmount = parsefloat($("#us").val());
 
     let norway = $("#NOK")[0].selected;
     let rwanda = $("#RWF")[0].selected;
@@ -17,6 +28,42 @@ $(document).ready(function(){
     let mexico = $("#MXN")[0].selected;
     let tanzania = $("#TZS")[0].selected;
     let other = $("#other")[0].selected;
+
+    let countryPicked;
+    let convension;
+    if(norway === true){
+      countryPicked = NOK;
+      ExchangeNow.getNorway()
+      .then(function(response){
+        conversion = Math.floor((usAmount)*(response.conversion_rates.countryPicked))
+        getNorwayElements(conversion, response);
+      });
+    }else if(rwanda === true){
+      countryPicked = RWF;
+      ExchangeNow.getRwanda()
+      .then(function(response){
+        getRwandaElements(conversion);
+      });
+    }else if(newZeland === true){
+      countryPicked = NZD;
+    }else if(mexico === true){
+      countryPicked = MXN;
+    }else if(tanzania === true){
+      countryPicked = TZS;
+    }else{
+      $("#error").show();
+    }
+
+
+
+    // ExchangeNow.getNorway()
+    //   .then(function(response){
+    //     getNorwayElements(response, countryPicked);
+    //   });
+    // ExchangeNow.getRwanda()
+    //   .then(function(response){
+    //     getRwandaElements(response, countryPicked);
+    //   });
 
   });
 });
