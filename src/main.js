c2fb47c2af3fcb7b3ceb319a204946f846ed099a
Sammai Gutierrez <sammaigutierrez@gmail.com>
2021-04-23 13:27:48 -0700

    Solve bugg by adding event.preventDefault().

--- src/main.js
+++ src/main.js
@@ -8,50 +8,56 @@ function getNorwayElements(conversion, response){
   if (conversion){
     $("#showConversion").text(`${conversion}`);
   } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
+    $('#showErrors').text(`There was an error: ${response.message}`);
   }
 }
-function getRwandaElements(){
 
-}
+// function getRwandaElements(){
+
+// }
 
 
-$(document).ready(function(){
-  $("form.exchanger").submit(function(event){
+
+  $("#go").on("click", (event) => {
     event.preventDefault();
+    console.log("whats going on!?");
 
-    let usAmount = parsefloat($("#us").val());
+    let usAmount = parseFloat($("#us").val());
 
     let norway = $("#NOK")[0].selected;
     let rwanda = $("#RWF")[0].selected;
     let newZeland = $("#NZD")[0].selected;
     let mexico = $("#MXN")[0].selected;
     let tanzania = $("#TZS")[0].selected;
-    let other = $("#other")[0].selected;
+    // let other = $("#other")[0].selected;
+    // $('#showConversion').val("");
 
-    let countryPicked;
-    let convension;
+    
+  
     if(norway === true){
-      countryPicked = NOK;
+      // countryPicked = "NOK";
+      $('#showConversion').val("");
       ExchangeNow.getNorway()
       .then(function(response){
-        conversion = Math.floor((usAmount)*(response.conversion_rates.countryPicked))
+        let x = response.conversion_rates.NOK
+        let conversion = Math.floor(usAmount * x);
+        console.log(conversion);
         getNorwayElements(conversion, response);
       });
     }else if(rwanda === true){
-      countryPicked = RWF;
-      ExchangeNow.getRwanda()
-      .then(function(response){
-        getRwandaElements(conversion);
-      });
+      // countryPicked = "RWF";
+      // ExchangeNow.getRwanda()
+      // .then(function(response){
+      //   getRwandaElements(conversion);
+      // });
     }else if(newZeland === true){
-      countryPicked = NZD;
+      // countryPicked = "NZD";
     }else if(mexico === true){
-      countryPicked = MXN;
+      // countryPicked = "MXN";
     }else if(tanzania === true){
-      countryPicked = TZS;
+      // countryPicked = "TZS";
     }else{
-      $("#error").show();
+      $("#ShowErrors").show();
     }
 
 
@@ -66,5 +72,5 @@ $(document).ready(function(){
     //   });
 
   });
-});
+
 
